d87bacba20d9761c7609ae09739d5c9d4f3ebd57
Juned <79745275+junaid11P@users.noreply.github.com>
2025-02-11 17:27:32 +0530

    ARQ

--- ARQ in DLL.c
+++ ARQ in DLL.c
@@ -1,115 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
-void StopAndWait();
-void GoBackN();
-void SelectiveRepeat();
+#define TOTAL_FRAMES 20
 
-int main(int argc, char** argv) {
-    int val;
-    while (1) {
-        printf("\nEnter:\n1. Stop And Wait protocol\n2. Go back N protocol\n3. Selective Repeat protocol\n");
-        scanf("%d", &val);
-        switch (val) {
-            case 1: StopAndWait(); break;
-            case 2: GoBackN(); break;
-            case 3: SelectiveRepeat(); break;
-            default: return 0;
-        }
-    }
-    return EXIT_SUCCESS;
-}
-
-// Function to implement Stop and Wait ARQ algorithm
-void StopAndWait() {
-    int n, i = 0, frame;
-    printf("Enter the total number of frames: ");
-    scanf("%d", &n);
-    while (i != n) {
-        printf("\nEnter received frame: ");
-        scanf("%d", &frame);
-        if (frame == i + 1) {
-            printf("Transmitting..... ACK to frame %d\n", frame);
-            i++;
-        } else {
-            printf("Negative ACK.... to frame %d\n", i + 1);
-        }
+void stopAndWait(int totalFrames){
+    printf("\nStop and Wait ARQ:\n");
+    for(int i=0;i<totalFrames; i++){
+        printf("transmitting frame %d\n", i+1);
+        printf("waiting for ack...\n");
+        printf("received ack for frame %d\n", i+1);
     }
 }
 
-// Function to implement Go back N algorithm
-void GoBackN() {
-    int n, i = 0, frame, size, t;
-    printf("Enter the total number of frames: ");
-    scanf("%d", &n);
-    printf("Enter window size of frames: ");
-    scanf("%d", &size);
-    printf("Sending frames: ");
-    for (int j = 0; j < size; j++) {
-        printf("%d, ", j + 1);
-    }
-    for (int j = 0; j < n; j++) {
-        printf("\nIs frame %d received (1 or 0): ", j + 1);
-        scanf("%d", &t);
-        if (t == 1) {
-            printf("Sending ACK to frame %d\n", j + 1);
-            printf("Sliding window: ");
-            for (int k = j + 1; k < j + 1 + size; k++) {
-                if (k < n)
-                    printf("%d, ", k + 1);
-            }
-        } else {
-            printf("\nRetransmitting frames: ");
-            for (int m = j; m < j + size; m++) {
-                if (m < n)
-                    printf("%d, ", m + 1);
-            }
+void goBackN(int totalFrames, int windowSize){
+    int nextFrameToSend = 0;
+    printf("\nGo Back N ARQ(Window Size = %d)\n", windowSize);
+
+    while(nextFrameToSend < totalFrames){
+        printf("Window: DSending frames %d to %d\n", nextFrameToSend+1, nextFrameToSend+windowSize);
+
+        for(int i=0; i<windowSize && nextFrameToSend < totalFrames; i++){
+            printf("transmitting frame %d\n", nextFrameToSend+1);
+            nextFrameToSend++;
         }
+
+        printf("waiting for ack...\n");
+        for(int i=0; i<windowSize && nextFrameToSend-i >0; i++){
+            printf("received ack for frame %d\n", nextFrameToSend-i);
+    }
+}
+}
+
+
+void selectiveRepeat(int totalFrames, int windowSize){
+    int nextFrameToSend = 0, nextFrameExpected =0;
+    printf("\nSelective Repeat ARQ(Window Size = %d)\n", windowSize);
+
+    while(nextFrameToSend < totalFrames){
+        printf("Window: Sending frames %d to %d\n", nextFrameToSend+1, nextFrameToSend+windowSize);
+
+        for(int i=0; i<windowSize && nextFrameToSend < totalFrames; i++){
+            printf("transmitting frame %d\n", nextFrameToSend+1);
+            nextFrameToSend++;
+        }
+
+        printf("waiting for ack...\n");
+        for(int i=nextFrameExpected; i<nextFrameToSend; i++){
+            printf("received ack for frame %d\n", i+1);
+        }
+        nextFrameExpected = nextFrameToSend;
+
     }
 }
 
-// Function to implement Selective Repeat algorithm
-void SelectiveRepeat() {
-    int n, m, size;
-    printf("Enter the total number of frames: ");
-    scanf("%d", &n);
-    printf("Sending frames: ");
-    for (int j = 0; j < n; j++) {
-        printf("%d, ", j + 1);
-    }
-    printf("\nEnter the number of frames received: ");
-    scanf("%d", &m);
-    int a[n];
-    printf("\nEnter received frames: ");
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &a[i]);
-    }
-    for (int i = 0; i < n; i++) {
-        int trigger = 0;
-        for (int j = 0; j < m; j++) {
-            if (i + 1 == a[j]) {
-                trigger = 1;
-                break;
-            }
-        }
-        if (!trigger) {
-            printf("Retransmitting frame %d\n", i + 1);
-            a[m++] = i + 1;
-        }
-    }
-    // Sorting the received frames
-    for (int i = 0; i < m - 1; i++) {
-        for (int j = i + 1; j < m; j++) {
-            if (a[i] > a[j]) {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-    }
-    printf("\nSorted frames: ");
-    for (int i = 0; i < m; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
+int main(){
+    int windowSize;
+
+    stopAndWait(TOTAL_FRAMES);
+
+    windowSize = 5;
+    goBackN(TOTAL_FRAMES, windowSize);
+
+    windowSize = 5;
+    selectiveRepeat(TOTAL_FRAMES, windowSize);
 }
